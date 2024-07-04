a50bfe3ec4a2fd916dad940c44e1d88300d6aa62
sefiSB <balintsch7@gmail.com>
2024-07-04 21:09:19 +0200

    small changes

--- src/Components/NewPlan/NewPlan.jsx
+++ src/Components/NewPlan/NewPlan.jsx
@@ -1,7 +1,32 @@
-function NewPlan(){
-    return(<>
-        <p>new plan</p>
-    </>);
+function NewPlan() {
+  return (
+    <>
+      <>
+        <div className="container pt-10">
+          <form class="max-w-sm mx-auto">
+            <div>
+              <label
+                for="small-input"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Title
+              </label>
+              <input
+                type="text"
+                id="small-input"
+                class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+            </div>
+            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Green to blue
+              </span>
+            </button>
+          </form>
+        </div>
+      </>
+    </>
+  );
 }
 
 export default NewPlan;
